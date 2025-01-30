d75a28d1249073cd23bd9153789af19a972f916e
technicalxakash <akashr2468@gmail.com>
2025-01-30 23:44:51 +0530

    intro and operators

---
+++ a.java
@@ -0,0 +1,106 @@
+// public class a{
+// public static void main(String[] args) {
+
+//     System.out.println("akash indians");
+    
+// }
+// }
+
+// import java.util.Scanner;
+
+// public class a{
+//     public static void main(String[] args) {
+//         Scanner sc=new Scanner(System.in);
+//         System.out.println("enter your name");
+//         String str=sc.nextLine();
+
+//         System.out.println("enter your name:"+str);
+//         sc.close();
+//     }
+//     }
+    
+// public class a{
+// public static void main(String[] args) {
+//     // int s = 10;
+//     //     s=70;
+//     //  int a,b=30,c;
+//     //  a=10;
+//     //  c=40;
+//     // int a=10;
+//     // int a=10;
+//     // double a=10.111111;
+
+
+//     System.out.println(a=100);
+    
+// }
+// }
+
+// public class a{
+// public static void main(String[] args) {
+
+//     int a=10;
+//     int b=10;
+    //int c=30;
+//arthematic operator
+    // System.out.println(a+b);
+    // System.out.println(a-b);
+    // System.out.println(a*b);
+    // System.out.println(a/b);
+    // System.out.println(a%b);
+
+//relational operator
+    // System.out.println(a==b);
+    // System.out.println(a<=b);
+    // System.out.println(a>=b);
+    // System.out.println(a>b);
+    // System.out.println(a<b);
+    // System.out.println(a!=b);
+//logical operators
+    // boolean x=true;
+    // boolean y=false;
+
+    //       System.out.println(x&&y);
+    //       System.out.println(x||y);
+    //       System.out.println(!x);
+    //       System.out.println(!y);
+//assignment opreator
+
+    // System.out.println(a=b);
+    // System.out.println(a-=b);
+    // System.out.println(a+=b);
+    // System.out.println(a*=b);
+    // System.out.println(a%=b);
+//increment operatro
+            // int d=10;
+            // d++;
+            // System.out.println(d);
+//decrement operator
+        // int e=10;
+        // e--;
+        // System.out.println(e);   
+// }
+// }
+
+import java.util.Scanner;
+
+
+
+public class a{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter number : ");
+        int a=sc.nextInt();
+        System.out.println("you entered number is : "+a);
+        sc.close();
+        
+        
+        if (a>5){
+         System.out.println("5 is lesser than 10");
+        }
+        else {
+            System.out.println("5 is greater than 10");
+         }
+        
+    }
+}
