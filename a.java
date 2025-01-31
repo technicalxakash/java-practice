235cefea2cae21e9e8960d114d01fa817c4e6c2a
technicalxakash <akashr2468@gmail.com>
2025-01-31 22:00:27 +0530

    looping satatements

--- a.java
+++ a.java
@@ -82,25 +82,110 @@
 // }
 // }
 
+// import java.util.Scanner;
+
+
+
+// public class a{
+//     public static void main(String[] args) {
+//         Scanner sc=new Scanner(System.in);
+//         System.out.println("enter number : ");
+//         int a=sc.nextInt();
+//         System.out.println("you entered number is : "+a);
+//         sc.close();
+        
+        
+//         if (a>5){
+//          System.out.println("5 is lesser than 10");
+//         }
+//         else {
+//             System.out.println("5 is greater than 10");
+//          }
+        
+//     }
+// }
 import java.util.Scanner;
 
-
-
 public class a{
-    public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        System.out.println("enter number : ");
-        int a=sc.nextInt();
-        System.out.println("you entered number is : "+a);
-        sc.close();
+    public static void main(String[] args){
+        //  Scanner sc=new Scanner(System.in);
+        // System.out.println("enter your name");
+        // String a=sc.nextLine();
+        // System.out.println("your name is "+a);
+        // sc.close();
+
+        // Scanner sc=new Scanner(System.in);
+        // System.out.print("enter value for a : ");
+        // int a=sc.nextInt();
+        // System.out.print("enter value for b : ");
         
+        // int b=sc.nextInt();
+        // System.out.println("you are entered value of a is "+a +" and b is "+b);
         
-        if (a>5){
-         System.out.println("5 is lesser than 10");
-        }
-        else {
-            System.out.println("5 is greater than 10");
-         }
-        
+        //  sc.close();
+
+        // if (a>b){
+        //     System.out.println(a+" is greater than "+b);
+        // }
+        // else if(a<b){
+        //     System.out.println(b+" is greater than "+a);
+        // }
+        // else{
+        //     System.out.println(a+" both are equal "+b);
+        // }
+    //    Scanner sc = new Scanner(System.in);
+    //    System.out.print("enter the day : ");
+    //    int day=sc.nextInt();
+      
+    //    sc.close();
+    //    switch (day) {
+    //     case 1: System.out.println("monday");      
+    //         break;
+    //     case 2: System.out.println("tuesday");           
+    //         break;
+    //     case 3: System.out.println("wed") ;          
+    //         break;
+    //     case 4: System.out.println("thur");           
+    //         break;
+    //     case 5: System.out.println("fri");           
+    //         break;
+    //     case 6: System.out.println("sat");           
+    //         break;
+    //}
+     
+//looping satatements
+    //   Scanner sc = new Scanner(System.in);     
+    //   System.out.print("enter the number to loop : ");
+    //   int n=sc.nextInt();
+    //   sc.close();
+    //   while(n<=100){
+    //     System.out.println(n);
+    //     n=n+2;
+      //}
+     
+    //   int i=1;
+    //   do{
+    //     System.out.println(i);
+    //     i++;
+
+    //   }
+    //   while(i<=10);
+      
+    // for(int i=1; i<=10;i++){
+    //     System.out.println(i);
+    // }
+
+
+    // for(int i=7;i<=70;i=i+7){
+
+    //     System.out.println("7*i="+i);
+    // }
+
+    int n=7;
+    for(int i=1; i<=10;i++){
+        System.out.println(n+"*"+i+"="+n*i);
     }
-}
+
+       }
+
+    }
