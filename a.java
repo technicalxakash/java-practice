7af191165b6cb2d79b9205976e71478018903045
technicalxakash <akashr2468@gmail.com>
2025-02-10 22:55:39 +0530

    class

--- a.java
+++ a.java
@@ -435,3 +435,18 @@
 //     }
 // }
 
+// Create a Main class
+// public class a {
+//     int x;  // Create a class attribute
+  
+//     // Create a class constructor for the Main class
+//     public a() {
+//       x = 5;  // Set the initial value for the class attribute x
+//     }
+  
+//     public static void main(String[] args) {
+//       a myObj = new a(); // Create an object of class Main (This will call the constructor)
+//       System.out.println(myObj.x); // Print the value of x
+//     }
+//   }
+  
