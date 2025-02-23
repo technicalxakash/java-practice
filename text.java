f31931b94e36d5098d6b329d76fec35f4ac2f053
technicalxakash <akashr2468@gmail.com>
2025-02-23 23:04:51 +0530

    execption handling

--- text.java
+++ text.java
@@ -317,3 +317,23 @@
 // }
 
 
+// import java.io.*;
+
+// class test{
+//   // declareing the type of exception
+//   public static void findFile() throws IOException {
+
+//     // code that may generate IOException
+//     File newFile = new File("test.txt");
+//     FileInputStream stream = new FileInputStream(newFile);
+//   }
+
+//   public static void main(String[] args) {
+//     try {
+//       findFile();
+//     }
+//     catch (IOException e) {
+//       System.out.println(e);
+//     }
+//   }
+// }
