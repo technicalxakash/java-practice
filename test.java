3fbdaa56cd982f8831734bc8a85c5830fbf40b89
technicalxakash <akashr2468@gmail.com>
2025-02-26 22:24:33 +0530

    methods

--- test.java
+++ test.java
@@ -339,3 +339,18 @@
 // }
 
 
+// public class test{
+//   static void myMethod() {
+//     System.out.println("I just got executed!");
+//   }
+
+//   public static void main(String[] args) {
+//     myMethod();
+//     myMethod();
+//     myMethod();
+//   }
+// }
+
+// I just got executed!
+// I just got executed!
+// I just got executed!
