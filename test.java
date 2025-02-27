494e1b86f076e2d2639842f0334492068dd1b573
technicalxakash <akashr2468@gmail.com>
2025-02-27 23:53:13 +0530

    methods

--- test.java
+++ test.java
@@ -357,23 +357,23 @@
 
 
 
-public class Test {
-  // Static method
-  static void myStaticMethod() {
-    System.out.println("Static methods can be called without creating objects");
-  }
+// public class Test {
+//   // Static method
+//   static void myStaticMethod() {
+//     System.out.println("Static methods can be called without creating objects");
+//   }
 
-  // Public method
-  public void myPublicMethod() {
-    System.out.println("Public methods must be called by creating objects");
-  }
+//   // Public method
+//   public void myPublicMethod() {
+//     System.out.println("Public methods must be called by creating objects");
+//   }
 
-  // Main method
-  public static void main(String[] args) {
-    myStaticMethod(); // Call the static method
-    // myPublicMethod(); This would compile an error
+//   // Main method
+//   public static void main(String[] args) {
+//     myStaticMethod(); // Call the static method
+//     // myPublicMethod(); This would compile an error
 
-    Main myObj = new Main(); // Create an object of Main
-    myObj.myPublicMethod(); // Call the public method on the object
-  }
-}
+//     Main myObj = new Main(); // Create an object of Main
+//     myObj.myPublicMethod(); // Call the public method on the object
+//   }
+// }
